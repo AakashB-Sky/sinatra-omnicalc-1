cf9fcbb1854fa0dc325d611909d016639a981ad5
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-02-02 00:38:07 +0000

    changed to <dl> list

--- app.rb
+++ app.rb
@@ -10,8 +10,8 @@ get("/square/new") do
 end
 
 get("/square/results") do
-  @num = params.fetch("number").to_i
-  @num_squared = @num ** 2
+  num = params.fetch("number").to_f # might want to think of a way to only show decimal place if not whole number
+  @num_squared = (num ** 2).to_f
 
   erb(:square_results)
 end
